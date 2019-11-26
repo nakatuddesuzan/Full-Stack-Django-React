c29fa46fdb548dfda1c441c6e6ddb4808f6cd205
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-26 14:31:54 -0600

    add urls

--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from leadmanager.leads.models import Lead
+from .models import Lead
 
 # Lead serializer
 class LeadSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Lead
         fields = '__all__'
