43266108a90ea0819bbe9e9e9c028fd32ccf41e8
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-26 14:13:29 -0600

    create lead view set

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,13 @@
+from leadmanager.leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# lead Viewset
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
