d42a90194430fbf0822f1339ed8531853d3b9924
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-26 20:15:30 -0600

    create components

---
+++ leadmanager/frontend/src/componets/leads/Dshboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Form from "./Form";
+import Lead from "./Lead";
+
+export default function Dshboard() {
+  return (
+    <Fragment>
+      <Form />
+      <Lead />
+    </Fragment>
+  );
+}
